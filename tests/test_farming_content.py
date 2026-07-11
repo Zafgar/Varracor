0c12b3affe090af262cb95b33b596598740fb304
Zafgar <tonikeskitalo91@gmail.com>
2026-07-11 22:09:34 +0300

    Add farming content, NPC tool and potion tests

---
+++ tests/test_farming_content.py
@@ -0,0 +1,155 @@
+import os
+
+os.environ.setdefault("SDL_VIDEODRIVER", "dummy")
+os.environ.setdefault("SDL_AUDIODRIVER", "dummy")
+os.environ.setdefault("PYGAME_HIDE_SUPPORT_PROMPT", "1")
+
+import pygame
+
+pygame.init()
+pygame.display.set_mode((1, 1))
+
+# Importing BarracksMenu installs all farming layers in the same order as main.py.
+from menus.barracks_menu import BarracksMenu
+import citys.mucford.farming_expansion as farming
+from citys.mucford.farming_content import HERB_DATA, POTION_RECIPES
+from items.farm_potions import BitterleafTonic, MoonpetalElixir
+from items.tools.harvest_tools import GuildHarvestScythe, IronHarvestSickle
+from lore.world_data import MARKET_PRICES
+
+
+class DummyVFX:
+    def show_damage(self, *args, **kwargs):
+        pass
+
+    def create_falling_leaves(self, *args, **kwargs):
+        pass
+
+
+class DummyClock:
+    weather = "clear"
+
+
+class DummyManager:
+    def __init__(self):
+        self.inventory = {}
+        self.city_storage = {}
+        self.equipment_bag = []
+        self.npc_state = {"global": {"reputation": 0, "flags": {}}}
+        self.vfx = DummyVFX()
+        self.world_clock = DummyClock()
+
+
+class DummyHarvester:
+    def __init__(self, tool):
+        self.unlocked_skills = {"harvesting_1", "harvesting_2", "harvesting_3"}
+        self.equipment = {"main_hand": tool}
+        self.primary_weapon = tool
+        self.current_weapon = tool
+
+
+class DummyAI:
+    job = "Farmer"
+
+
+class DummyFarmer:
+    def __init__(self, name):
+        self.name = name
+        self.ai_controller = DummyAI()
+        self.inventory = []
+        self.weapon_masteries = set()
+
+
+class DummyFighter:
+    def __init__(self):
+        self.max_hp = 100
+        self.current_hp = 20
+        self.max_mana = 80
+        self.current_mana = 10
+        self.max_stamina = 90
+        self.current_stamina = 15
+        self.injured = True
+        self.injury_severity = "Minor"
+        self.equipment = {"usable": None, "usable2": None}
+
+
+def test_expanded_layout_has_twenty_valid_plots_and_named_herbs():
+    assert getattr(BarracksMenu, "_farm_alchemy_installed", False)
+    assert len(farming.PLOT_LAYOUT) == 20
+    assert len(HERB_DATA) == 7
+    for crop_name, _col, _row in farming.PLOT_LAYOUT:
+        assert crop_name in farming.CROP_DATA
+    for herb_name, herb in HERB_DATA.items():
+        assert herb_name in farming.CROP_DATA
+        assert herb["kind"] == "herb"
+        assert herb["potion_use"]
+
+
+def test_market_contains_all_harvest_tools_and_water():
+    expected = {
+        "Bucket of Water": "BucketWater",
+        "Crude Harvest Sickle": "CrudeHarvestSickle",
+        "Iron Harvest Sickle": "IronHarvestSickle",
+        "Guild Harvest Scythe": "GuildHarvestScythe",
+    }
+    for shop_name, class_name in expected.items():
+        assert MARKET_PRICES["buy"][shop_name]["class"] == class_name
+    for herb_name in HERB_DATA:
+        assert herb_name in MARKET_PRICES["sell"]
+
+
+def test_npc_harvest_places_real_product_in_city_storage_and_ledger():
+    manager = DummyManager()
+    state = {"growth_ticks": 0, "watered": True, "harvest_count": 0}
+    plot = farming.CropPlot(0, 0, "Moonpetal", state, "moonpetal_test")
+    plot.growth_ticks = plot.data["growth_frames"]
+    worker = DummyHarvester(GuildHarvestScythe())
+
+    assert plot.harvest(manager, worker, to_storage=True, npc=True)
+    assert manager.city_storage["Moonpetal"] >= 1
+    assert manager.npc_state["farming"]["npc_harvest_totals"]["Moonpetal"] >= 1
+    assert plot.growth_ticks == 0
+    assert plot.watered is False
+
+
+def test_farmer_roster_gets_all_required_tool_tiers():
+    manager = DummyManager()
+    farmers = [DummyFarmer(f"Farmer {index}") for index in range(5)]
+    system = object.__new__(farming.FarmingSystem)
+    system.manager = manager
+    system.city = type("City", (), {"npcs": farmers})()
+
+    system._equip_farmer_npcs()
+
+    tiers = []
+    for farmer in farmers:
+        tools = [item for item in farmer.inventory
+                 if getattr(item, "tool_type", "") == "harvest"]
+        assert len(tools) == 1
+        tiers.append(tools[0].tool_tier)
+        assert "harvest_tool" in farmer.weapon_masteries
+    assert sorted(tiers, reverse=True) == [3, 2, 2, 1, 1]
+
+
+def test_farm_potions_apply_effects_and_are_consumed_from_slot():
+    fighter = DummyFighter()
+    tonic = BitterleafTonic()
+    fighter.equipment["usable"] = tonic
+    assert tonic.cast(fighter)
+    assert fighter.current_hp == 45
+    assert fighter.equipment["usable"] is None
+
+    elixir = MoonpetalElixir()
+    fighter.equipment["usable2"] = elixir
+    assert elixir.cast(fighter)
+    assert fighter.current_mana == 46
+    assert fighter.equipment["usable2"] is None
+
+
+def test_every_alchemy_recipe_builds_a_concrete_potion():
+    assert len(POTION_RECIPES) >= 5
+    for recipe in POTION_RECIPES.values():
+        potion = recipe["factory"]()
+        assert potion.type == "potion"
+        assert potion.slot_type == "usable"
+        assert recipe["ingredients"]
